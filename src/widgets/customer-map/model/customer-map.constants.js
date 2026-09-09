18d7362f9d8207366146813da4fe26a62959edfd
ibeksapar <aibek.saparr@gmail.com>
2026-09-09 17:11:51 +0500

    fix: map public key

--- src/widgets/customer-map/model/customer-map.constants.js
+++ src/widgets/customer-map/model/customer-map.constants.js
@@ -11,5 +11,6 @@ export const driverIcon = L.divIcon({
 export const CUSTOMER_MAP_TILE_LAYER = {
    attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-   url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+   url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png?key=cb1_33bl_1_e67a46f1e67e6cbad8143c4e',
+   
 };
